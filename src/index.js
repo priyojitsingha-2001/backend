c1a84c80d839392c9835abc6494dda4e1859f513
Priyojit <priyojitsingha01@gmail.com>
2024-05-19 10:33:56 +0530

    app initialization complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import connectDb from "./db/db.js";
+import dotenv from "dotenv";
+import app from "./app.js";
 
-connectDb();
+dotenv.config();
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGO DB Connection failed !!!`, err);
+  });
