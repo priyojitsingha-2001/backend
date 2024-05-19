816672d5ae54a596c51480d863cec2e7e742bdec
Priyojit <priyojitsingha01@gmail.com>
2024-05-19 15:47:26 +0530

    express middlewares initialized

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -6,5 +6,8 @@ import cookieParser from "cookie-parser";
 const app = express();
 //configuring cors
 app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({}));
+app.use(express.static("public"));
 
-export default app;
+export { app };
