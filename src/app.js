91428f3a2c622d8c18e8b556e7533f9cdd0b7b87
Priyojit <priyojitsingha01@gmail.com>
2024-05-19 11:26:33 +0530

    cors configuration complete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// Initializing express
 const app = express();
+//configuring cors
+app.use(cors());
 
 export default app;
