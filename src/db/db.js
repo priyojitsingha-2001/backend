79b6540cb6fc26271ca8e78575147c83e7679fa6
Priyojit <priyojitsingha01@gmail.com>
2024-05-19 11:54:22 +0530

    adding comments to code

--- src/db/db.js
+++ src/db/db.js
@@ -2,17 +2,23 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 import dotenv from "dotenv";
 
+//load environment variables from .env file
 dotenv.config();
+//define an asynchronous function to connect to the MongoDB database
 const connectDb = async () => {
   try {
+    // Attempt to connect to the MongoDB database using the connection URI from environment variables
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
+    // Log a success message with the host of the connected database
     console.log(
       `\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
+    // Log an error message if the connection attempt fails
     console.log("MONGODB connection error", error);
+    // Exit the process with a failure status code
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import connectDb from "./db/db.js";
 import dotenv from "dotenv";
 import app from "./app.js";
 
+// Load environment variables from the .env file
 dotenv.config();
 
+// Call the connectDb function to connect to the MongoDB database
 connectDb()
   .then(() => {
+    // If the database connection is successful, start the Express server
     app.listen(process.env.PORT || 8000, () => {
+      // Log a message indicating that the server is running and listening on the specified port
       console.log(`Server running at PORT ${process.env.PORT}`);
     });
   })
   .catch((err) => {
+    // If the database connection fails, log an error message
     console.log(`MONGO DB Connection failed !!!`, err);
   });
